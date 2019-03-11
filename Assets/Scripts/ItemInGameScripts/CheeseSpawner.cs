e8ec259e1168e7c0365bf4915865c7270870d0fc
nurakmaljalil91 <arc_mole@yahoo.com>
2019-03-11 17:21:21 +0800

    Fix npc movement

---
+++ Assets/Scripts/ItemInGameScripts/CheeseSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseSpawner : MonoBehaviour {
+
+	public GameObject cheeseLocation;
+
+	public GameObject cheesePrefab;
+	private GameObject cheese;
+
+	private GameObject[] cheeseLocationInArray;
+
+
+
+	private bool cheeseIsInMap;
+
+	private Vector3 randomPosition;
+
+	// Use this for initialization
+	void Start () {
+		InitTheArrayWithTheLocationOfCheese();
+		cheeseIsInMap = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// check if there is a cheese in the map
+		if(!cheeseIsInMap){
+			// TODO: Generate the random position location
+			GenerateRandomLocation();
+			// TODO: we spawn the cheese in the random position
+			SpawnTheCheese();
+		
+		}
+
+		// TODO: Here is where we destroy the cheese and change the boolean
+	}
+
+	void InitTheArrayWithTheLocationOfCheese(){
+
+		cheeseLocationInArray = new GameObject[transform.childCount]; 
+		for(int i = 0; i < transform.childCount; i++){
+			cheeseLocationInArray[i] = transform.GetChild(i).gameObject;
+		}
+	}
+
+	void GenerateRandomLocation(){
+		int randomNumber = Random.Range(0, transform.childCount);
+
+		randomPosition = cheeseLocationInArray[randomNumber].transform.position;
+	}
+
+	void SpawnTheCheese(){
+
+		// Instatiate the cheese
+		cheese = Instantiate(cheesePrefab, randomPosition, Quaternion.identity);
+		cheeseIsInMap = true;
+	}
+
+}
--- Assets/Scripts/NPCScripts/NPCMovement.cs
+++ Assets/Scripts/NPCScripts/NPCMovement.cs
@@ -4,87 +4,117 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
 
-public class NPCMovement : MonoBehaviour {
+public class NPCMovement : MonoBehaviour
+{
 
-	private enum STATE {idle, moving};
-	private STATE state;
-	public Text randomPositionText;
+    private enum STATE { idle, moving };
+    private STATE state;
+    public Text randomPositionText;
 
-	public Text positionInArray;
+    public Text positionInArray;
 
-	// Nav Mesh agent of the npc
-	private NavMeshAgent agent;
+    // Nav Mesh agent of the npc
+    private NavMeshAgent agent;
 
-	// target for the npc to moveS
-	// maybe inside the array random location
-	private Transform target;
+    // target for the npc to moveS
+    // maybe inside the array random location
+    private Transform target;
 
-	private int randomNumber;
+    private int randomNumber;
 
-	public GameObject randomLocation;
+    public GameObject randomLocation;
 
-	private Transform[] randomLocationPosition;
+    private Transform[] randomLocationPosition;
 
-	// Use this for initialization
-	void Start () {
-		agent = GetComponent<NavMeshAgent>();
-		// Init the random location	
-		InitRandomLocation();
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        // Init the random location	
+        InitRandomLocation();
 
-		// Init the state as idle
-		state = STATE.idle;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        // Init the state as idle
+        state = STATE.idle;
 
-		// TODO: Check the state of the npc
-		// if it not moving it will start generate the random number
-		if(state == STATE.idle){
-			// if it moving no need to generate random number
-		// TODO: get random number
-			randomNumber = GenerateRandomNumber();
-		
-		}
-		
-		
-		// get random location
-		ChooseTheLocation();
-		// move to the random location
-		//FIXME: movement is weird
-		MoveToTarget();
+    }
 
-		DebugRandomPositionText();
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-	// generate the random number
-	int GenerateRandomNumber(){
-		int randomNumber = Random.Range(0, 6);
+        // TODO: Check the state of the npc
+        // if it not moving it will start generate the random number
+        if (state == STATE.idle)
+        {
+            // if it moving no need to generate random number
+            // TODO: get random number
+            randomNumber = GenerateRandomNumber();
+            // after generating the random number
+            // the npc will start choose the location
+            ChooseTheLocation();
+            // move to the random location
+            //FIXME: movement is weird
+            MoveToTarget();
 
-		return randomNumber;
-	}
+        }
+        if (state == STATE.moving)
+        {
+            //check if the npc arrived at the target position
+            ArrivedAtTargetPosition();
+        }
+
+   
+        DebugRandomPositionText();
+    }
+
+    // generate the random number
+    int GenerateRandomNumber()
+    {
+        int generateNumber = Random.Range(0, randomLocation.transform.childCount);
+
+        return generateNumber;
+    }
 
 
-	void InitRandomLocation(){
+    void InitRandomLocation()
+    {
 
-		randomLocationPosition = new Transform[randomLocation.transform.childCount];
+        randomLocationPosition = new Transform[randomLocation.transform.childCount];
 
-		for(int i = 0; i < randomLocation.transform.childCount; i++){
-			randomLocationPosition[i] = randomLocation.transform.GetChild(i).transform;
-		}
-	}
+        for (int i = 0; i < randomLocation.transform.childCount; i++)
+        {
+            randomLocationPosition[i] = randomLocation.transform.GetChild(i).transform;
+        }
+    }
 
-	void ChooseTheLocation(){
-		target = randomLocationPosition[GenerateRandomNumber()];
-	}
+    void ChooseTheLocation()
+    {
+        target = randomLocationPosition[randomNumber];
+    }
 
-	void MoveToTarget(){
-		agent.SetDestination(target.position);
-	}
+    void MoveToTarget()
+    {
+        agent.destination = target.position;
+        state = STATE.moving;
+    }
 
-	void DebugRandomPositionText(){
-		randomPositionText.text = "NPC position: " + target.position.ToString();
-	}
+    void DebugRandomPositionText()
+    {
+        randomPositionText.text = "NPC position: " + transform.position.ToString();
+    }
+
+    void ArrivedAtTargetPosition()
+    {
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    state = STATE.idle;
+                }
+            }
+        }
+    }
 
 }
