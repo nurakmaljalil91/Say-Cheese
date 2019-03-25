41f48e9803a2caf537242b3b5508f60dcad128cf
nurakmaljalil91 <arc_mole@yahoo.com>
2019-03-25 14:59:41 +0800

    Still implement the shop system

---
+++ Assets/Scripts/MainMenuScripts/ShopControllerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopControllerScript : MonoBehaviour {
+ 
+	public int totalGameGolds;
+
+	[SerializeField] private GameObject[] items;
+	// Use this for initialization
+	void Start () {
+
+		items = new GameObject[transform.childCount];
+
+		for(int i = 0; i < transform.childCount; i++){
+			items[i] = transform.GetChild(i).gameObject;
+
+		}
+
+		// This for testing so the game gold will be hundred
+		totalGameGolds = 100;
+		// totalGameGolds = PlayerPrefs.GetInt("Gold");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void BuyItem(){
+		// Check if the money enough to buy the item
+		// if enough the button will change to equip
+		
+	}
+}
